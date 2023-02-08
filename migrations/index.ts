fc089becbc31ad75e6dba56464805fb44b26c374
Roman <brysommer1@gmail.com>
2023-02-08 13:41:57 +0200

    new commit

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,14 +1,15 @@
 import { StatusEnum } from '../config/enum';
 import { Status, createStatus } from '../models/statuses';
+import { Channel } from '../models/channels';
 import { logger } from '../logger';
 import { Log } from '../models/logs';
 
 
-const pseudoRandom = () => Math.floor(Math.random() * 10000);
-Status.sync().then(() => createStatus(pseudoRandom(), StatusEnum.off));
+// const pseudoRandom = () => Math.floor(Math.random() * 10000);
+// Status.sync().then(() => createStatus(pseudoRandom(), StatusEnum.off));
 
-Log.sync().then(() => logger.info('Log created by migration procedure'));
+//Log.sync().then(() => logger.info('Log created by migration procedure'));
 
-// Channel.sync();
+Channel.sync();
 
 // Pinger.sync();
