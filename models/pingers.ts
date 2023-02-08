ff8db351cdc21dff3bc24031ccee74ef12222dde
Victor <vbanudz@glenflow.com>
2023-02-08 21:07:20 +0200

    upd

--- models/pingers.ts
+++ models/pingers.ts
@@ -1,22 +1,11 @@
-// + PingerEnum, 
-// + addNewIp (channelId, ip)
-// + find.One readIpsByChannelId (channelId) 
-// + deleteIpByChannelId (channelId)
-// + updateIpByChannelId (channelId, ip)
-// + find.All readIpsByChannelId (channelId) 
-
-import { Sequelize, Model, DataTypes } from 'sequelize';
-import { StatusEnum, PingerType } from '../config/enum';
-
-const sequelize = new Sequelize({
-    storage: __dirname + '/pb.db',
-    dialect: 'sqlite'
-});
+import { Model, DataTypes } from 'sequelize';
+import { PingerType } from '../config/enum';
+import { sequelize } from './sequelize';
 
 class Pinger extends Model {}
 Pinger.init({
     type: {
-        type: DataTypes.ENUM(...Object.values(StatusEnum)),
+        type: DataTypes.ENUM(...Object.values(PingerType)),
         allowNull: false
     },
     channel_id: {
@@ -41,58 +30,49 @@ interface PingerFields {
     value: string
 }
 
-const readOneIpByChannelId = async (channel_id: number): Promise<PingerFields|null> => {
-    const res = await Pinger.findOne({ where: { channel_id: channel_id }});
-    //console.log(res?.dataValues.value);
-    if (res) return res.dataValues.value;
-    return res;
+const findIpsByChannelId = async (channel_id: number): Promise<PingerFields[]|undefined> => {
+    try {
+        const res = await Pinger.findAll({ where: { channel_id: channel_id } });
+        //console.log(res);
+        return res.map((pinger: Pinger) => pinger.dataValues.value);
+    } catch (error) {
+        console.error(`Error reading IPs by channel id: ${error}`);
+        throw error; // було return [];
+    }
 };
 
-//ось цю давай розберемо разом не розумію що в ній не так, походу видаляє на тесті
-  async function deleteNewChannelId(channel_id: number) {
+const updateIpByPingerId = async (id: number, ip: string): Promise<PingerFields|undefined> => {
     try {
-      const pingerId = await Pinger.destroy({ where: { channel_id: channel_id } });
-      console.log(`Successfully deleted channel id: ${channel_id}`);
+        const updated = await Pinger.update({ value: ip }, { where: { id }, returning: true });
+        if (updated) {
+            console.log(`Successfully updated channel id: ${id}`);
+        } else {
+            console.error(`Error updating channel id: ${id} not found`);
+        }
     } catch (error) {
-      console.error(`Error deleting channel id: ${error}`);  
+        console.error(`Error updating channel id: ${error}`);
     }
-  };  
+};
 
-  //назва функції "Оновити Ід Каналу" при цьому якесь newValue передається
-  //тут поміняв назву
-  const updateIpByChannelId = async (channel_id: number, newValue: string) => {
+async function deleteIpByPingerId(id: number): Promise<Boolean> {
     try {
-    const updated = await Pinger.update({ value: newValue }, { where: { channel_id: channel_id }, returning: true });
-    if (updated) {
-    console.log(`Successfully updated channel id: ${channel_id}`);
-    } else {
-    console.error(`Error updating channel id: ${channel_id} not found`);
-    }
+        const pingerId = await Pinger.destroy({ where: { id } });
+        console.log(`Successfully deleted channel id: ${id}`);
     } catch (error) {
-    console.error(`Error updating channel id: ${error}`);
+        console.error(`Error deleting channel id: ${error}`);  
     }
-    };
+};  
 
-  const readIpsByChannelId = async (channel_id: number): Promise<PingerFields[]> => {
-    try {
-      const res = await Pinger.findAll({ where: { channel_id: channel_id } });
-      //console.log(res);
-      return res.map((pinger: Pinger) => pinger.dataValues.value);
-    } catch (error) {
-      console.error(`Error reading IPs by channel id: ${error}`);
-      throw error; // було return [];
-    }
-  };
 
-//readIpsByChannelId(1);
-//deleteNewChannelId(2);
-//updateIpByChannelId(5, '44.20.3.1');
-//readIpsByChannelId(5);
-
+// addNewIp ('192.168.11.22', 234234)
+// findIpsByChannelId(234234),
+// updateIpByPingerId(2, '192.168.11.22'),
+// deleteIpByPingerId(2)
+    
 export {
     Pinger,
-    readOneIpByChannelId,
-    deleteNewChannelId,
-    updateIpByChannelId,
-    readIpsByChannelId,
+    // addNewIp
+    findIpsByChannelId,
+    updateIpByPingerId,
+    deleteIpByPingerId
 };
